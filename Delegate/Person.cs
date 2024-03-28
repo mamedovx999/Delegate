02eba086eb74e306172dfb8f02757200b6935ebc
mamedovx999 <mamedovx999@code.edu.az>
2024-03-28 04:14:47 +0400

    Add project files.

---
+++ Delegate/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+ public class Person
+    {
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Adress { get; set; }
+        public int Salary { get; set; }
+
+
+
+        public Person(string name, string surname, string adress, int salary)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Adress = adress;
+            this.Salary = salary;
+        }
+    }
+}
---
+++ Delegate/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<Person> persons = new List<Person>
+            //{
+            //new Person("Memmedeli", "Taghiyev", "Baki", 1200),
+            //new Person("Akif", "Subhanov", "Rusiya", 800),
+            //new Person("Davud", "Tagiyev", "Baki", 600),
+            //new Person("Emil", "Vusalov", "Sumqayit",1500)
+            //};
+            //ShowPerson(persons,p => p.Salary > 1000 );
+
+            //static void ShowPerson(List<Person> persons, Predicate<Person> predicate)
+            //{
+            //    foreach (var person in persons)
+            //    {
+            //        if (predicate(person))
+            //        {
+            //            Console.WriteLine($"Ad: {person.Name}, Soyad: {person.Surname}, Ünvan: {person.Adress}");
+            //        }
+            //    }
+            //}
+
+            List<Book> books = new List<Book>
+            {
+                new Book("Xosrov ve sirin ","Nizami"),
+                new Book("sevgi","Albert"),
+                new Book("Iskendername ","Nizami"),
+                new Book("Tenha Baki ","Memmedeli"),
+                new Book("Leyli ve mejnun ","Nizami")
+            };
+
+            int count = BookCount(books, b => b.Author == "Nizami");
+            Console.WriteLine($"Nizaminin Kitab Sayi {count}");
+
+            static int BookCount(List<Book>books,Predicate<Book> predicate)
+            {
+                int count = 0;
+                foreach (Book book in books)
+                    if (predicate(book))
+                    {
+                    count++;
+                    }
+                     return count;
+            } 
+        }
+    }
+}
+
